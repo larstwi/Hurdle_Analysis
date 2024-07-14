2f39fc54077a06b04db7c7ee4a7f1023a02d7761
Your Name <you@example.com>
2024-07-14 03:35:16 +0200

    updated data

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import pandas as pd
 # Funktion zum Laden der Daten
 @st.cache
 def load_data():
-    data = pd.read_excel('data/Analyse2.xlsx')
+    data = pd.read_excel('data/Analyse3.xlsx')
     return data
 
 
