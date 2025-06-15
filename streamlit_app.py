6b5391dd85350e832094f4440ffff6f605221b97
Your Name <you@example.com>
2025-06-16 01:12:41 +0200

    changed filter

--- streamlit_app.py
+++ streamlit_app.py
@@ -81,7 +81,7 @@ def main():
     selected_columns = filtered_data.iloc[:, [4, 5, 8, 11, 14, 18, 21, 24, 27, 30, 33]]
 
     # Create a new column 'index' representing the row index
-    selected_columns['Wettkampf'] = filtered_data["Wettkampf"]
+    selected_columns['Wettkampf'] = filtered_data["Name"] + " - " + filtered_data["Wettkampf"]
 
    # Get the column names (excluding 'index')
     columns_order = selected_columns.columns[:-1].tolist()
