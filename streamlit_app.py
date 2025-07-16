5e33b609ee3cd25110840ec14acfea601d5803b0
Your Name <you@example.com>
2025-07-16 23:54:02 +0200

    luzern

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import altair as alt
 # Funktion zum Laden der Daten
 @st.cache_data
 def load_data():
-    data = pd.read_excel('data/Analyse18.xlsx')
+    data = pd.read_excel('data/Analyse19.xlsx')
     return data
 
 
